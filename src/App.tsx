632d0cc2821cdef22acfb82eaa911fb91fc60890
MJ Ansari <ansarimj14@gmail.com>
2024-05-12 20:45:07 +0545

    new component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import './App.css'
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Provider } from 'react-redux';
+import store from './store/store';
 function App() {
 
   return (
     <>
-      <h1> Hello</h1>
+      <Provider store={store}>
+
+        <Router>
+          <Routes>
+            <Route path="/" element={<h1> Hello</h1>} />
+            <Route path="/login" element={<h1> Login</h1>} />
+          </Routes>
+        </Router>
+      </Provider>
     </>
   )
 }
--- src/store/blogSlice.ts
+++ src/store/blogSlice.ts
@@ -1,25 +1,34 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 
-interface User {
-    username: string,
+interface Blog {
+    title: string,
+    subtitle: string,
+    description: string,
 
 }
 
-
-const initialState = {
-    user: {
-
-    }
+interface BlogState {
+    blog: Blog,
+    status: string
 }
 
-const authSlice = createSlice({
-    name: "author",
-    initialState: {},
+const initialState: BlogState = {
+    blog: {} as Blog,
+    status: "loading"
+}
+
+const blogSlice = createSlice({
+    name: 'blog',
+    initialState,
     reducers: {
-        setuser(state, action) { }
-        setStatus(state, action) { }
-    })
+        setUser(state: BlogState, action: PayloadAction<Blog>) {
+            state.blog = action.payload
+        },
+        setStatus(state: BlogState, action: PayloadAction<string>) {
+            state.status = action.payload
+        }
+    }
+})
 
-
-export const { setuser, setStatus } = authSlice.actions
-export default authSlice.reducer
+export const { setUser, setStatus } = blogSlice.actions
+export default blogSlice.reducer
