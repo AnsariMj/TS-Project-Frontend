b4fdaf4102565ddf9c811a5b614b8c801c46729b
MJ Ansari <ansarimj14@gmail.com>
2024-05-16 00:17:56 +0545

    Regiter Login function added to store

---
+++ src/http/index.ts
@@ -0,0 +1,9 @@
+import axios from "axios";
+const API = axios.create({
+    baseURL: 'http://localhost:3000/',
+    headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+    }
+})
+export default API;
--- src/store/authSlice.ts
+++ src/store/authSlice.ts
@@ -1,4 +1,16 @@
 import { PayloadAction, createSlice } from '@reduxjs/toolkit'
+import API from '../http'
+
+interface RegisterData {
+    username: string,
+    email: string,
+    password: string,
+}
+
+interface LoginData {
+    email: string,
+    password: string,
+}
 
 interface User {
     username: string,
@@ -33,3 +45,36 @@ const authSlice = createSlice({
 
 export const { setUser, setStatus } = authSlice.actions
 export default authSlice.reducer
+
+
+export function regiter(data: RegisterData) {
+    return async function registerThunk(dispatch: any) {
+        dispatch(setStatus('loading'));
+        try {
+            const response = await API.post("register", data);
+            if (response.status === 201) {
+                dispatch(setStatus("success"));
+            } else {
+                dispatch(setStatus("error"));
+            }
+        } catch (error) {
+            dispatch(setStatus("error"));
+        }
+    }
+}
+
+export function login(data: LoginData) {
+    return async function loginThunk(dispatch: any) {
+        dispatch(setStatus("loading"));
+        try {
+            const response = await API.post("login", data);
+            if (response.status === 200) {
+                dispatch(setStatus("success"));
+            } else {
+                dispatch(setStatus("error"));
+            }
+        } catch (error) {
+            dispatch(setStatus('error'))
+        }
+    }
+}
